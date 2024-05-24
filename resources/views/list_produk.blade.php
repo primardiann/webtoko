4ee51c3c9b2cbc664d035f8de2a3c42bba9241d3
Prima Ardiansyah <ardiansyahprima2005@gmail.com>
2024-05-24 22:30:21 +0700

    updte prak 9

--- resources/views/list_produk.blade.php
+++ resources/views/list_produk.blade.php
@@ -1,26 +1,44 @@
-<div>
-    <h1>INPUT PRODUK</h1>
-</div>
-<form method="POST" action="{{ route('produk.simpan') }}">
-    @csrf
-    <table class="table">
-        <tr>
-            <td>Nama:</td>
-            <td colspan="3">
-                <input type="text" class="form-control" id="nama" name="nama"></td>
-            </tr>
-            <tr>
-                <td>Deskripsi:</td>
-                <td colspan="3">
-                    <textarea class="form-control" name="deskripsi" id="deskripsi"></textarea>
-                </td>
-            </tr>
-            <tr>
-                <td>Harga:</td>
-                <td>
-                    <input type="number" class="form-control" id="harga" name="harga"></td>
-                </tr>
+<link
+    href="https://cdn.jsdelivr.net/npm/daisyui@4.11.1/dist/full.min.css"
+    rel="stylesheet"
+    type="text/css"/>
+<script src="https://cdn.tailwindcss.com"></script>
 
-            </table>
-            <button type="submit" class="btn btn-primary">Simpan</button>
-        </form>
+<h2 class="text-xl">
+    INPUT PRODUK
+    <br>
+
+<div>
+    <form method="POST" action="{{ route('produk.simpan') }}">
+        @csrf
+        <table class="table-pin-rows">
+            <tr>
+                <td>Nama:</td>
+                <td colspan="3">
+                    <input
+                        type="text"
+                        class="input input-bordered input-primary w-full max-w-xs"
+                        id="nama"
+                        name="nama"></td>
+                </tr>
+                <tr>
+                    <td>Deskripsi:</td>
+                    <td colspan="3">
+                        <textarea class="textarea textarea-primary" name="deskripsi" id="deskripsi"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Harga:</td>
+                    <td>
+                        <input
+                            type="number"
+                            class="input input-bordered input-primary w-full max-w-xs"
+                            id="harga"
+                            name="harga"></td>
+                    </tr>
+
+                </table>
+                <br>
+                    <button type="submit" class="btn btn-outline">Simpan</button>
+                </form>
+            </div>
